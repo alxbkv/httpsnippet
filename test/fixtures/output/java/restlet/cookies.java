a6b3531c5734e96f8d372ad75d2512e37b50f81e
Nicolas Baptiste <nbaptiste-ext@restlet.com>
2015-08-27 17:08:59 +0200

    feat(tests): add test for restlet cookies snippet

---
+++ test/fixtures/output/java/restlet/cookies.java
@@ -0,0 +1,9 @@
+ClientResource cr = new ClientResource("http://mockbin.com/har");
+cr.getCookies().add(new Cookie("foo", "bar"));
+cr.getCookies().add(new Cookie("bar", "baz"));
+try {
+    Representation representation = cr.post(null);
+    System.out.println(representation.getText());
+} catch (ResourceException e) {
+    System.err.println("Status: " + e.getStatus() + ". Response: " + cr.getResponse().getEntityAsText());
+}
